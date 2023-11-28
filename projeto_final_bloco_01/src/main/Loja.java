4f9058044a13fc79d9ed401b7b036feba8c21743
Joao Pedro Oliveira Rochaq <pedrorocha2info@gmail.com>
2023-11-27 23:41:51 -0300

    repository pronto

--- projeto_final_bloco_01/src/main/Loja.java
+++ projeto_final_bloco_01/src/main/Loja.java
@@ -27,7 +27,7 @@ public class Loja {
 			case 1 -> cadastrarProdutos();
 			case 2 -> listarProdutos();
 			case 3 -> comprarProdutos();
-			case 4 -> verCarrinho();
+			case 4 -> finalizarCompra();
 			case 5 -> { 
 				System.out.println("Saindo do programa");
 				System.exit(0);
@@ -48,79 +48,31 @@ public class Loja {
 		System.out.println(produto.getNome() + " Cadastrato com sucesso");
 		menu();
 	}	
-
-    private static void listarProdutos() {
-        String resultado = controller.listarProdutos();
-        System.out.println(resultado);
-        menu();
-    }
-
+	
+	private static void listarProdutos() {
+	    System.out.println(controller.listarProdutos(produtos));
+	    menu();
+	}
+	
 	private static void comprarProdutos() {
-		if (produtos.size() > 0) {
-			System.out.println("codigo do produtos! \n");
-			System.out.println("Produtos Disponiveis \n");
-			for(Produto p : produtos) {
-				System.out.println(p + "\n");
-			}       
-			int id = Integer.parseInt(input.next());
-			boolean isPresent = false;
-			for (Produto p : produtos) {
-				if (p.getId() == id) {
-					int qtd = 0;
-					try {
-						qtd = carrinho.get(p);
-						carrinho.put(p, qtd +1);
-					}catch (NullPointerException e) {
-						carrinho.put(p, 1);
-					}
-					System.out.println(p.getNome() + " adiconado ao carrinho" );
-					isPresent = true;
-					break; // break the loop once the product is found
-            }
-        }
-        if (!isPresent) {
-            System.out.println("Eletronico ou peca nao encontrada");
-            menu();
-        } else {
-            System.out.println("Adicionar outro eletronico, 1-sim, 2-nao");
-            int option = Integer.parseInt(input.next());
-            if (option == 1) {
-                comprarProdutos();
-            }else {
-                finalizarCompra();
-            }
-        }
-    } else {
-        System.out.println("Eletronico ou peça ainda nao cadastrado");
-        menu();
-    }   
-}
-
-	private static void verCarrinho() {
-		System.out.println("Eletronico no carrinho de pedidos");
-		if (carrinho.size() > 0) {
-			for ( Produto p : carrinho.keySet()) {
-				System.out.println("Eletronicos: " + p + "\nQuantidades: " + carrinho.get(p));
-			}
-		}else {
-			System.out.println("Carrinho vazio");
-		}
-		menu();
+	    System.out.println("Digite o ID do produto que deseja comprar:  ");
+	    int id = Integer.parseInt(input.next());
+	    controller.comprarProdutos(produtos, carrinho, id);
+	    
+	    System.out.println("Deseja continuar comprando? 1 - Sim, 2 - Não");
+	    int opcao = Integer.parseInt(input.next());
+	    if (opcao == 1) {
+	        comprarProdutos();
+	    } else {
+	        menu();
+	    }
 	}
+
 	private static void finalizarCompra() {
-		Double valorDaCompra = 0.0;
-		System.out.println("OS Pedidos: ");
-
-		for (Produto p : carrinho.keySet()) {
-			int qtd = carrinho.get(p);
-			valorDaCompra += p.getPreco() * qtd;
-			System.out.println(p);
-			System.out.println("Quantidades: " + qtd);
-		}
-		System.out.println("O valor da sua compra é: " + Utils.doubleToString(valorDaCompra));
-		carrinho.clear();
-		menu();
+	    controller.finalizarCompra(carrinho);
+	    menu();
 	}
+
 }
 
 
--- projeto_final_bloco_01/src/modelo/Controller.java
+++ projeto_final_bloco_01/src/modelo/Controller.java
@@ -1,24 +1,59 @@
 package modelo;
-import modelo.Produto;
 import java.util.List;
-import repository.Repository;
+import java.util.Map;
+
 import utils.Utils;
-import main.Loja;
 
 public class Controller {
     // Supondo que produtos seja uma lista de produtos
     private List<Produto> produtos;
 
-    public String listarProdutos() {
+    public String listarProdutos(List<Produto> produtos) {
         StringBuilder sb = new StringBuilder();
         if (produtos.size() > 0) {
             sb.append("Lista de produtos! \n");
-            for(Produto p : produtos) {
+            for(modelo.Produto p : produtos) {
                 sb.append(p.toString()).append("\n");
             }
         } else {
             sb.append("Nenhum Produto Cadastrado");
         }
         return sb.toString();
+    }
+    public void comprarProdutos(List<Produto> produtos, Map<Produto, Integer> carrinho, int id) {
+        if (produtos.size() > 0) {
+            System.out.println("Produtos Disponiveis \n");
+            for(Produto p : produtos) {
+                System.out.println(p + "\n");
+            }       
+            boolean isPresent = false;
+            for (Produto p : produtos) {
+                if (p.getId() == id) {
+                    int qtd = 0;
+                    try {
+                        qtd = carrinho.get(p);
+                        carrinho.put(p, qtd +1);
+                    }catch (NullPointerException e) {
+                        carrinho.put(p, 1);
+                    }
+                    System.out.println(p.getNome() + " adiconado ao carrinho" );
+                    isPresent = true;
+                    break; // break the loop once the product is found
+                }
+            }
+        }
+    }
+    public void finalizarCompra(Map<Produto, Integer> carrinho) {
+        Double valorDaCompra = 0.0;
+        System.out.println("OS Pedidos: ");
+
+        for (Produto p : carrinho.keySet()) {
+            int qtd = carrinho.get(p);
+            valorDaCompra += p.getPreco() * qtd;
+            System.out.println(p);
+            System.out.println("Quantidades: " + qtd);
+        }
+        System.out.println("O valor da sua compra é: " + Utils.doubleToString(valorDaCompra));
+        carrinho.clear();
     }
 }
